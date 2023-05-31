4c64d5160f31a603c56ff47f0b71bb84de4f2360
umidnm <umidnm@code.edu.az>
2023-05-31 12:38:14 +0400

    Shop Created

---
+++ PustokBackTask/ViewModels/ShopViewModel.cs
@@ -0,0 +1,13 @@
+﻿using PustokBackTask.Models;
+
+namespace PustokBackTask.ViewModels
+{
+    public class ShopViewModel
+    {
+        public List<Genre> Genres { get; set; }
+        public List<Author> Authors { get; set; }
+        public List<Tag> Tags { get; set; }
+        public List<Book> Books { get; set; }
+
+    }
+}
